031ad8247afd13af4675af94960d52d3311f5112
Gulnar Hagverdiyeva <glnrhagverdiyeva@gmail.com>
2024-07-21 22:04:59 +0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,41 +3,69 @@ import "../db.json";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import Loading from "./components/loading";
+import Search from "./components/search";
 function App() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
   const nagigate = useNavigate();
+  const [query, setQuery] = useState("");
   useEffect(() => {
     axios
       .get("http://localhost:3000/data")
-      .then((res) => setData(res.data))
+      .then((res) => {
+        setLoading(false);
+        setData(res.data);
+      })
       .catch((err) => console.log(err));
   }, []);
+  // console.log(data.filter(d.name.toLowerCase.includes("Li")));
+  // console.log(query);
+  console.log(data.filter((user) => user.name.toLowerCase().includes("li")));
   return (
     <>
+      {loading && <Loading />}
       <div className="wrapper">
         <h2>
           Add Product
-        <button className="button"><Link to="/create" className="button">
-            Add
-          </Link>
-          </button>  
+          <button className="button">
+            <Link to="/create" className="button">
+              Add
+            </Link>
+          </button>
+          {/* <Search/> */}
+          <div className="search">
+            <input
+              type="text"
+              placeholder="search"
+              onChange={(e) => setQuery(e.target.value)}
+            />
+          </div>
         </h2>
         <div className="products">
-          {data.map((d, i) => {
-            return (
-              <div className="product">
-                <img src={d.img} alt="" />
-                <h3>{d.name}</h3>
-                <p>{d.details}</p>
-              <button tton className="button"> <Link className="button" to={`/edit/${d.id}`}>
-                  Edit
-                </Link></button> 
-                <button onClick={(e) => handleSubmit(d.id)} className="button">
-                  Delete
-                </button>
-              </div>
-            );
-          })}
+          {data
+            .filter((user) => user.name.toLowerCase().includes(query))
+            .map((d, i) => {
+              return (
+                <div className="product">
+                  <img src={d.img} alt="" />
+                  <h3>{d.name}</h3>
+                  <p>{d.details}</p>
+                  <button tton className="button">
+                    {" "}
+                    <Link className="button" to={`/edit/${d.id}`}>
+                      Edit
+                    </Link>
+                  </button>
+                  <button
+                    onClick={(e) => handleSubmit(d.id)}
+                    className="button"
+                  >
+                    Delete
+                  </button>
+                </div>
+              );
+            })}
         </div>
       </div>
     </>
---
+++ src/components/search.jsx
@@ -0,0 +1,9 @@
+function Search() {
+  return (
+    <div className="search">
+      <input type="text" placeholder="search" onChange={e=>e.Set}/>
+    </div>
+  );
+}
+
+export default Search;
